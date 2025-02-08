db6b250502fd3d99e52fe16d9afe4884646886a3
kirurr <timon09df@gmail.com>
2025-02-08 11:09:20 +0300

    updated metadata

--- src/app/profile/update/page.tsx
+++ src/app/profile/update/page.tsx
@@ -7,7 +7,7 @@ import { redirect } from "next/navigation";
 import { Suspense } from "react";
 
 export const metadata = {
-  title: "Update your data",
+  title: "Update user data",
 };
 
 export default async function page() {
