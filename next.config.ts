833ef59f4ca19ed4fc0fa0d612f0fa23daa9b196
kirurr <timon09df@gmail.com>
2025-02-09 03:20:30 +0300

    updated config file

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,10 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   reactStrictMode: true,
+  reactComponentAnnotation: {
+    enabled: process.env.NODE_ENV === 'development',
+  },
+ 
 };
 
 export default nextConfig;
